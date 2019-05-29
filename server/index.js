af615044e6692f2f35118c44e4501bde3dd008ba
Gregory Maxedon <gregory.maxedon@gmail.com>
2019-05-29 10:27:44 -0700

    added port environment variable

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const port = process.env.PORT || 9000
 
 app.use(express.static(path.join(__dirname, '../build')));
 
@@ -9,4 +10,4 @@ app.get('/*', function (req, res) {
 });
 
 console.log('server started');
-app.listen(9000);
+app.listen(port);
